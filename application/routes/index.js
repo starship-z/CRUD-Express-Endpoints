bdc41dc8533bc083c2b3aa1bfc3a612b3fd5ee76
Ana Nytochka <70048765+starship-z@users.noreply.github.com>
2021-05-22 22:48:00 -0700

    Add api endpoint

--- application/routes/index.js
+++ application/routes/index.js
@@ -2,6 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// localhost:3000/api/ana
+router.get('/api/ana', function(req, res, next) {
+  res.send({status: true, name: "ana"})
+});
+
+// localhost:3000/
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
